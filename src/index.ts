4c919ce29347231e8b1e452cbd18919d8bd5c625
AhmadT14 <ahragh@gmail.com>
2026-02-19 11:47:13 +0200

    set up project database and implement [register, reset, users] management commands

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,25 @@
 import { registerCommand, runCommand } from "./configCommands";
 import {CommandsRegistry} from "./configCommands";
-import { handlerLogin } from "./loginConfigCommand";
-
+import { handlerLogin } from "./loginCommand";
 import { argv } from "node:process";
+import { handlerRegister } from "./registerCommand";
+import { handlerReset } from "./resetCommand";
+import { handlerUsers } from "./usersCommand";
 
-function main() {
+async function main() {
   const commandsRegistry: CommandsRegistry = {};
-  registerCommand(commandsRegistry, "login", handlerLogin);
+  registerCommand(commandsRegistry, "login",  handlerLogin);
+  registerCommand(commandsRegistry, "register", handlerRegister);
+  registerCommand(commandsRegistry, "reset", handlerReset);
+  registerCommand(commandsRegistry, "users", handlerUsers);
 
   if (argv.length > 2) {
-    runCommand(commandsRegistry, argv[2], ...argv.slice(3));
+    await runCommand(commandsRegistry, argv[2], ...argv.slice(3));
   } else {
     console.error("Not enough arguments provided.");
     process.exit(1);
   }
+  process.exit(0);
 }
 
-main();
+await main();
---
+++ src/registerCommand.ts
@@ -0,0 +1,16 @@
+import { setUser } from "./config";
+import { createUser, getUserByName } from "./lib/db/queries/users";
+
+export async function handlerRegister(cmdName: string, ...args: string[]) {
+  if (args.length === 0) {
+    console.error("No name provided.");
+    process.exit(1);
+  }
+  if(await getUserByName(args[0])){
+    console.error("User already exists.");
+    process.exit(1);
+  }
+   await createUser(args[0]);
+  setUser(args[0]);
+  console.log("User registered and set to: " + args[0]);
+}
